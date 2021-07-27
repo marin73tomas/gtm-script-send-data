c0b7deefd3e5f5ba3025adcce010565fae4ff2bc
= <=>
2021-07-27 14:07:34 -0400

    fixed initfingerprintjs

--- public/external.js
+++ public/external.js
@@ -3,6 +3,17 @@ function sendAnalyticsData(elapsedTime, analyticsData, url = "/log") {
     elapsedTime,
     ...analyticsData,
   };
+  if (!data.visitorId) {
+    const fpPromise = FingerprintJS.load();
+
+    // Get the visitor identifier when you need it.
+    fpPromise
+      .then((fp) => fp.get())
+      .then((result) => {
+        // This is the visitor identifier:
+        data.visitorId = result.visitorId;
+      });
+  }
   console.log(`sending data to ${url}:`, data);
   var blob = new Blob([JSON.stringify(data)], {
     type: "application/json; charset=UTF-8",
